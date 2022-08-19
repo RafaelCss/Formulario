982da4b77c35affbb2f49acefc4ea802a2a15ff2
devspacerj <rf2014souza@gmail.com>
2022-08-19 10:29:36 -0300

    19/08/2022 criando conexão com fire base

---
+++ firebases.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfjInOWLPhFbeeKBEspHSS770REFllBl8",
+  authDomain: "cadastro-d5ed0.firebaseapp.com",
+  databaseURL: "https://cadastro-d5ed0-default-rtdb.firebaseio.com",
+  projectId: "cadastro-d5ed0",
+  storageBucket: "cadastro-d5ed0.appspot.com",
+  messagingSenderId: "221429861650",
+  appId: "1:221429861650:web:619b1a06a9dd75f9d5f7cb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
