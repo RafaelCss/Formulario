5e1c637871c935be16a3a5208715517103aee41f
devspacerj <rf2014souza@gmail.com>
2022-08-21 22:11:20 -0300

    ajuste na app

--- .next/static/MJjHB9GVa3S2P12xLqFSR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/759-16fa768cf95cb155.js","static/chunks/pages/index-a5b46d4c706f6e16.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
